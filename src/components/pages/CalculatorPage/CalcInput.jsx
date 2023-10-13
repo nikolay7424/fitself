e46ada9882e789d719461164c2cea8f3539de263
nikolay7424 <nikolay7424@gmail.com>
2023-10-13 17:51:39 +0300

    state mess

--- src/components/pages/CalculatorPage/CalcInput.jsx
+++ src/components/pages/CalculatorPage/CalcInput.jsx
@@ -1,4 +1,60 @@
-function CalcInput({ inputType, inputLabel, inputValue }) {
+import { useState } from "react"
+
+function CalcInput({
+    inputType,
+    inputLabel,
+    inputValue,
+    genders,
+    setInputGender,
+    calculateCalorieIntake,
+    setInputValue,
+    limit,
+    calculateBmi,
+}) {
+    const [fatPercentage, setFatPercentage] = useState("5%")
+
+    function handleGenderInputBoolean(e, chosenGender) {
+        e.preventDefault()
+        setInputGender(chosenGender)
+        Array.from(e.target.parentElement.children).forEach((button) => {
+            button.classList.remove("btn-active")
+        })
+        e.target.classList.add("btn-active")
+        calculateCalorieIntake()
+    }
+    function handleRangeInput(e) {
+        setInputValue(Number(e.target.value))
+        calculateCalorieIntake()
+        calculateBmi()
+    }
+    function handleRadioInput(e) {
+        setInputValue(Number(e.target.getAttribute("data-value")))
+        switch (inputValue) {
+            case 1.1:
+                setFatPercentage("5%")
+                break
+            case 1.15:
+                setFatPercentage("15%")
+                break
+            case 1.2:
+                setFatPercentage("20%")
+                break
+            case 1.25:
+                setFatPercentage("30%")
+                break
+            case 1.3:
+                setFatPercentage("40+%")
+                break
+        }
+
+        Array.from(e.target.parentElement.parentElement.children).forEach(
+            (img) => {
+                img.children[0].style.opacity = 0.6
+            }
+        )
+        e.target.style.opacity = 1
+        calculateCalorieIntake()
+    }
     return (
         <>
             {(() => {
@@ -8,13 +64,23 @@ function CalcInput({ inputType, inputLabel, inputValue }) {
                             <>
                                 <div className="calc__input">
                                     <button
-                                        onClick={(e) => e.preventDefault()}
+                                        onClick={(e) =>
+                                            handleGenderInputBoolean(
+                                                e,
+                                                genders.male
+                                            )
+                                        }
                                         className="btn btn-active"
                                     >
                                         Male
                                     </button>
                                     <button
-                                        onClick={(e) => e.preventDefault()}
+                                        onClick={(e) =>
+                                            handleGenderInputBoolean(
+                                                e,
+                                                genders.female
+                                            )
+                                        }
                                         className="btn"
                                     >
                                         Female
@@ -36,10 +102,11 @@ function CalcInput({ inputType, inputLabel, inputValue }) {
                                         className="calc__input--range"
                                         type="range"
                                         min={1}
-                                        max={200}
+                                        max={limit}
                                         name="age"
                                         id="age"
                                         defaultValue={inputValue}
+                                        onChange={(e) => handleRangeInput(e)}
                                     />
                                     <span className="calc__input--value">
                                         {inputValue}
@@ -56,23 +123,53 @@ function CalcInput({ inputType, inputLabel, inputValue }) {
                                     </label>
                                     <fieldset>
                                         <div className="calc__input--radio">
-                                            <img src="../../../src/assets/img/fat/1.svg" />
+                                            <img
+                                                onClick={(e) =>
+                                                    handleRadioInput(e)
+                                                }
+                                                data-value="1.1"
+                                                src="../../../src/assets/img/fat/1.svg"
+                                            />
                                         </div>
                                         <div className="calc__input--radio">
-                                            <img src="../../../src/assets/img/fat/2.svg" />
+                                            <img
+                                                onClick={(e) =>
+                                                    handleRadioInput(e)
+                                                }
+                                                data-value="1.15"
+                                                src="../../../src/assets/img/fat/2.svg"
+                                            />
                                         </div>
                                         <div className="calc__input--radio">
-                                            <img src="../../../src/assets/img/fat/3.svg" />
+                                            <img
+                                                onClick={(e) =>
+                                                    handleRadioInput(e)
+                                                }
+                                                data-value="1.2"
+                                                src="../../../src/assets/img/fat/3.svg"
+                                            />
                                         </div>
                                         <div className="calc__input--radio">
-                                            <img src="../../../src/assets/img/fat/4.svg" />
+                                            <img
+                                                onClick={(e) =>
+                                                    handleRadioInput(e)
+                                                }
+                                                data-value="1.25"
+                                                src="../../../src/assets/img/fat/4.svg"
+                                            />
                                         </div>
                                         <div className="calc__input--radio">
-                                            <img src="../../../src/assets/img/fat/5.svg" />
+                                            <img
+                                                onClick={(e) =>
+                                                    handleRadioInput(e)
+                                                }
+                                                data-value="1.3"
+                                                src="../../../src/assets/img/fat/5.svg"
+                                            />
                                         </div>
                                     </fieldset>
                                     <span className="calc__input--value">
-                                        {inputValue}
+                                        {fatPercentage}
                                     </span>
                                 </div>
                             </>
@@ -84,13 +181,15 @@ function CalcInput({ inputType, inputLabel, inputValue }) {
                                     <label className="calc__input--label">
                                         {inputLabel}
                                     </label>
-                                    <select name="activity" id="activity">
-                                        <option value="low">Low</option>
-                                        <option value="medium">Medium</option>
-                                        <option value="high">High</option>
-                                        <option value="very-high">
-                                            Very high
-                                        </option>
+                                    <select
+                                        onChange={(e) => handleRangeInput(e)}
+                                        name="activity"
+                                        id="activity"
+                                    >
+                                        <option value="1.2">Low</option>
+                                        <option value="1.4">Medium</option>
+                                        <option value="1.6">High</option>
+                                        <option value="1.8">Very high</option>
                                     </select>
                                 </div>
                             </>
--- src/components/pages/CalculatorPage/CalcPage.jsx
+++ src/components/pages/CalculatorPage/CalcPage.jsx
@@ -21,26 +21,83 @@ export default function CalcPage() {
         activity: "Activity",
     }
 
+    const inputRangeLimits = {
+        age: 100,
+        height: 250,
+        weight: 200,
+    }
+
+    const genders = {
+        male: {
+            cal: 660,
+            weightMultiplier: 13.7,
+            heightMultiplier: 5,
+            ageMultiplier: 6.8,
+        },
+        female: {
+            cal: 655,
+            weightMultiplier: 9.6,
+            heightMultiplier: 1.8,
+            ageMultiplier: 4.7,
+        },
+    }
+
+    const [inputGender, setInputGender] = useState(genders.male)
+    const [inputAgeValue, setInputAgeValue] = useState(50)
+    const [inputHeightValue, setInputHeightValue] = useState(150)
+    const [inputWeightValue, setInputWeightValue] = useState(80)
+    const [inputFatValue, setInputFatValue] = useState(1.1)
+    const [inputActivityValue, setInputActivityValue] = useState(1)
+
     //  bmi = inputWeightValue / (inputHeightValue/100 * inputHeightValue/100)
-    let bmi = 0
+    const [bmi, setBmi] = useState(0)
     // BMI less than 18.5: bmiIndicatorIndex = 0
     // BMI from 18.5 to 24.9: bmiIndicatorIndex = 1
     // BMI from 25.0 to 29.9: bmiIndicatorIndex = 2
     // BMI from 30.0 to 34.9: bmiIndicatorIndex = 3
     // BMI from 35.0 to 39.9: bmiIndicatorIndex = 4
     // BMI 40.0 and above: bmiIndicatorIndex = 5
-    let bmiIndicatorIndex = 0
+    const [bmiIndicatorIndex, setBmiIndicatorIndex] = useState(0)
 
-    // calorieIntake for male = (660 + (13.7 * inputWeightValue) + (5 * inputHeightValue) - (6.8 * setInputAgeValue)) * inputActivityValue[1.2-1.8 шаг 0.2] * inputFatValue[1.1-1.3 шаг 0.05]
+    const [calorieIntake, setCalorieIntake] = useState(0)
 
-    // calorieIntake for female =655 + (9.6 * inputWeightValue) + (1.8 * inputHeightValue) - (4.7 * inputAgeValue) * inputActivityValue[1.2-1.8 шаг 0.2] * inputFatValue[1.1-1.3 шаг 0.05]
-    let calorieIntake = 0
+    function calculateCalorieIntake() {
+        setCalorieIntake(
+            Math.round(
+                (inputGender.cal +
+                    inputGender.weightMultiplier * inputWeightValue +
+                    inputGender.heightMultiplier * inputHeightValue -
+                    inputGender.ageMultiplier * inputAgeValue) *
+                    inputActivityValue *
+                    inputFatValue
+            )
+        )
+    }
 
-    const [inputAgeValue, setInputAgeValue] = useState(50)
-    const [inputHeightValue, setInputHeightValue] = useState(150)
-    const [inputWeightValue, setInputWeightValue] = useState(80)
-    const [inputFatValue, setInputFatValue] = useState(5)
-    const [inputActivityValue, setInputActivityValue] = useState(1)
+    function calculateBmi() {
+        setBmi(
+            Number(
+                Math.round(
+                    (inputWeightValue /
+                        (((inputHeightValue / 100) * inputHeightValue) / 100)) *
+                        10
+                ) / 10
+            )
+        )
+        if (bmi < 18.5) {
+            setBmiIndicatorIndex(0)
+        } else if (bmi > 18.5 && bmi < 24.9) {
+            setBmiIndicatorIndex(1)
+        } else if (bmi > 25 && bmi < 29.9) {
+            setBmiIndicatorIndex(2)
+        } else if (bmi > 30 && bmi < 34.9) {
+            setBmiIndicatorIndex(3)
+        } else if (bmi > 35 && bmi < 39.9) {
+            setBmiIndicatorIndex(4)
+        } else {
+            setBmiIndicatorIndex(5)
+        }
+    }
 
     return (
         <>
@@ -48,31 +105,53 @@ export default function CalcPage() {
                 <div className="main__calc--wrapper">
                     <h2>Body weight calculator</h2>
                     <form action="#">
-                        <CalcInput inputType={inputType.boolean} />
+                        <CalcInput
+                            genders={genders}
+                            setInputGender={setInputGender}
+                            inputType={inputType.boolean}
+                            calculateCalorieIntake={calculateCalorieIntake}
+                        />
                         <CalcInput
                             inputType={inputType.range}
                             inputLabel={inputLabels.age}
                             inputValue={inputAgeValue}
+                            setInputValue={setInputAgeValue}
+                            calculateCalorieIntake={calculateCalorieIntake}
+                            calculateBmi={calculateBmi}
+                            limit={inputRangeLimits.age}
                         />
                         <CalcInput
                             inputType={inputType.range}
                             inputLabel={inputLabels.height}
                             inputValue={inputHeightValue}
+                            setInputValue={setInputHeightValue}
+                            calculateCalorieIntake={calculateCalorieIntake}
+                            calculateBmi={calculateBmi}
+                            limit={inputRangeLimits.height}
                         />
                         <CalcInput
                             inputType={inputType.range}
                             inputLabel={inputLabels.weight}
                             inputValue={inputWeightValue}
+                            setInputValue={setInputWeightValue}
+                            calculateCalorieIntake={calculateCalorieIntake}
+                            calculateBmi={calculateBmi}
+                            limit={inputRangeLimits.weight}
                         />
                         <CalcInput
                             inputType={inputType.radio}
                             inputLabel={inputLabels.fat}
                             inputValue={inputFatValue}
+                            setInputValue={setInputFatValue}
+                            calculateCalorieIntake={calculateCalorieIntake}
                         />
                         <CalcInput
                             inputType={inputType.select}
                             inputLabel={inputLabels.activity}
                             inputValue={inputActivityValue}
+                            setInputValue={setInputActivityValue}
+                            calculateCalorieIntake={calculateCalorieIntake}
+                            calculateBmi={calculateBmi}
                         />
                     </form>
                 </div>
